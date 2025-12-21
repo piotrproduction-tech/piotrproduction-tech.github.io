b888b3f10ae98933054049f510f3f5a95fc7d64b
piotrjacko <piotr.dgproduction@gmail.com>
2025-12-21 21:01:24 +0100

    Update and rename modules/finance/placeholder1.txt to modules/finance/finance/finance-menu.js

---
+++ modules/finance/finance/finance-menu.js
@@ -0,0 +1,8 @@
+// ===============================================
+// CityOfGATE — FINANCE MENU (FE‑02)
+// WERSJA 1.0
+// ===============================================
+
+export function initModule() {
+    SystemLog.log("FinanceMenu", "info", "Moduł Finance Menu został uruchomiony");
+}
