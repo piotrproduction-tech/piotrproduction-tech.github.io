9ee23c45f58e492ce65b74798b0a8d0cd96e2fd7
piotrjacko <piotr.dgproduction@gmail.com>
2025-12-21 21:49:48 +0100

    Delete modules/finance/finance/finance-menu.js

--- modules/finance/finance/finance-menu.js
+++
@@ -1,8 +0,0 @@
-// ===============================================
-// CityOfGATE — FINANCE MENU (FE‑02)
-// WERSJA 1.0
-// ===============================================
-
-export function initModule() {
-    SystemLog.log("FinanceMenu", "info", "Moduł Finance Menu został uruchomiony");
-}
